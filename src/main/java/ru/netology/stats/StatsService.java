468067acab7b1ab6559f5bbb6331bec611bebeef
Nika <gviliya2012@mail.ru>
2023-04-27 18:21:14 +0300

    src

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,62 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+
+        //   for (int i = 0; i < sales.length; i++) {
+        // totalSale += sales[i];
+        // }
+
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int monthMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMaximum]) {
+                monthMaximum = i;
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int monthMinimumSale(long[] sales) {
+        int monthMinimum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[monthMinimum]) {
+                monthMinimum = i;
+            }
+        }
+        return monthMinimum + 1;
+    }
+
+    public int monthsBellowAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if(sale<average(sales)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int monthsHigherAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if(sale>average(sales)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
